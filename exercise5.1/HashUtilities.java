6d3c098b81107d3418014f9c42d0345fc44c6a56
BBK-PiJ-2016-22 <caspertellerup@gmail.com>
2016-10-26 18:18:19 +0100

    first commit

--- exercise5.1/HashUtilities.java
+++ exercise5.1/HashUtilities.java
@@ -2,7 +2,7 @@ class HashUtilities{
 	
 	public static int shortHash(int number){
 			
-		int number2 = number % 3;
+		int number2 = number % 1000;
 	
 		return Math.abs(number2);
 		
---
+++ exercise5.2/Maps.java
@@ -0,0 +1,29 @@
+public class Maps implements SimpleMap{
+
+
+	public void put(int key, String name){
+	
+	
+	}
+
+	
+	public String get(int key){
+		String hi = "hello";
+		return hi;
+	}
+
+	
+	
+	public void remove(int key){
+	
+	}
+
+	
+	public boolean isEmpty(){
+	
+		return false;
+	} 
+
+
+
+}
